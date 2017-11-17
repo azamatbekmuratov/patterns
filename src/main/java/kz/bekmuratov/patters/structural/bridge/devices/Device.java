347acf679a61af6d04d8e18da8c8b7e5b2a7ea36
a.bekmuratov <azamat.bekmuratov@gmail.com>
2017-11-17 09:35:57 +0600

    - добавил паттерн мост - added bridge pattern

---
+++ src/main/java/kz/bekmuratov/patters/structural/bridge/devices/Device.java
@@ -0,0 +1,20 @@
+package kz.bekmuratov.patters.structural.bridge.devices;
+
+public interface Device {
+    public boolean isEnabled();
+
+    public void enable();
+
+    public void disable();
+
+    public int getVolume();
+
+    public void setVolume(int percent);
+
+    public int getChannel();
+
+    public void setChannel(int channel);
+
+    public void printStatus();
+
+}
