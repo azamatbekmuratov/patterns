09e97a6314ad30fc03bde6b775cbfa20dfa998b3
a.bekmuratov <azamat.bekmuratov@gmail.com>
2017-11-21 09:02:02 +0600

    - добавил шаблон декоратор - added decorator pattern

---
+++ src/main/java/kz/bekmuratov/patters/structural/decorator/decorators/DataSource.java
@@ -0,0 +1,8 @@
+package kz.bekmuratov.patters.structural.decorator.decorators;
+
+/* Интерфейс, задающий базовые операции чтения и записи данных */
+public interface DataSource {
+    void writeData(String data);
+
+    String readData();
+}
